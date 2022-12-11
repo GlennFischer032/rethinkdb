dbd5b53e423f3f1f1dc05d845e60e812aabe764a
GlennFischer032 <glennfischer032@gmail.com>
2022-12-11 18:30:25 +0100

    JOIN

--- example.py
+++ example.py
@@ -1,5 +1,15 @@
 from rethinkdb import RethinkDB
 
 r = RethinkDB()
-r.connect("localhost", 49157).repl()
-print(r.table('countries').run())
+r.connect("localhost", 49154).repl()
+
+
+# JOIN
+# r.db('fakt').table('neviem_alkohol').index_create('Entity').run()
+# r.db('fakt').table('neviem').index_create('country').run()
+# print(r.db('fakt').table('neviem').eq_join('country', r.db('fakt').table('neviem_alkohol'), index="Entity").run())
+
+res = r.db('fakt').table('neviem').run()
+for item in res:
+    print(item)
+
