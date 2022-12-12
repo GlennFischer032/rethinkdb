42a5fb211e4026ce908711004d0de1f48d04db90
GlennFischer032 <glennfischer032@gmail.com>
2022-12-12 21:08:32 +0100

    j

--- example.py
+++ example.py
@@ -1,13 +1,14 @@
 from rethinkdb import RethinkDB
 
 r = RethinkDB()
-r.connect("localhost", 49154).repl()
+r.connect("localhost", 49157).repl()
 
 db = r.db('fakt')
+
 # JOIN
 # db.table('neviem_alkohol').index_create('Entity').run()
 # db.table('neviem').index_create('country').run()
-# print(db.table('neviem').eq_join('country', db.table('neviem_alkohol'), index="Entity").run())
+#print(db.table('neviem').eq_join('country', db.table('neviem_alkohol'), index="Entity").run())
 
 
 # GEO QUERY
@@ -15,7 +16,9 @@ db = r.db('fakt')
 # for item in res:
 #     if 'latitude' not in item or 'longitude' not in item:
 #         continue
-#     db.table('neviem').get(item['id']).update({'location': r.point(float(item["latitude"]), float(item["longitude"]))}).run()
+#     db.table('neviem').get(item['id']).update({'location': r.point(float(item["longitude"]), float(item["latitude"]))}).run()
 
-# db.table('neviem').index_create('location', geo=True).run()
-# print(db.table('neviem').get('eb73be08-02d7-4aad-aa3d-4acb4aa43d21')['location'].distance(db.table('neviem').get('ddc5c8de-8f1e-4ec3-adbf-2411078c7bb6')['location']).run())
+# print(db.table('neviem').run())
+# print(db.table('neviem').get_nearest(r.point(8.981666, 17.00778), index='location', max_results=10, unit='km').run())
+# print(db.table('neviem').get('11017e65-3fc7-44a7-b0f1-2062a662c30e')['location'].distance(db.table('neviem').get('0d4cbf7e-933c-43dc-9626-6571c5d99ce4')['location'], unit='km').run())
+
