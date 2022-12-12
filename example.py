eccaed5f38ebd41811419f75396a260da201ecd7
GlennFischer032 <glennfischer032@gmail.com>
2022-12-12 21:21:51 +0100

    f

--- example.py
+++ example.py
@@ -18,8 +18,6 @@ print(db.table('country_locations').eq_join('country', db.table('country_alcohol
 res = db.table('country_locations').run()
 # create location field
 for item in res:
-    if 'latitude' not in item or 'longitude' not in item:
-        continue
     db.table('country_locations').get(item['id']).update({'location': r.point(float(item["longitude"]), float(item["latitude"]))}).run()
 
 # create geosptaial index
@@ -28,6 +26,8 @@ db.table('country_locations').index_create('location', geo=True).run()
 #find the nearest country to the coordinates
 print(db.table('country_locations').get_nearest(r.point(8.981666, 17.00778), index='location').run())
 
-#disctance between the United States and the Czech Republic =~ 8228 km
-print(db.table('country_locations').get('11017e65-3fc7-44a7-b0f1-2062a662c30e')['location'].distance(db.table('neviem').get('0d4cbf7e-933c-43dc-9626-6571c5d99ce4')['location'], unit='km').run())
+# disctance between the United States(id=11017e65-3fc7-44a7-b0f1-2062a662c30e)
+# and the Czech Republic(id=0d4cbf7e-933c-43dc-9626-6571c5d99ce4) =~ 8228 km
+print(db.table('country_locations').get('11017e65-3fc7-44a7-b0f1-2062a662c30e')['location']
+      .distance(db.table('neviem').get('0d4cbf7e-933c-43dc-9626-6571c5d99ce4')['location'], unit='km').run())
 
